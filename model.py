31408b6e17edbfe15afc876f10489d65e1baa518
Ashwin_Pokharel <pokharel.ashwin1@gmail.com>
2021-12-28 21:49:44 -0500

    updated with resnet encoder summary and requirements

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
-#defines the base model class
-
+#defines the base model class this will be using a unet model as defined in the paper
+import torch
+from torchinfo import summary
+from torch.nn import Model
 
--- train.py
+++ train.py
@@ -1,1 +1,6 @@
 #This will contain the training code for the diffusion model
+import functools
+import os
+from .nn import update_ema
+
+INITIAL_LOG_LOSS_SCALE = 20.0
